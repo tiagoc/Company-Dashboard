06313e6a6f1dce9f14af2d76e383fa6c5979ac81
diogoeiras <eiras@outlook.com>
2014-11-27 17:36:03 +0000

    Página de clientes melhorada; página de visualização de documentos criada

---
+++ FirstREST/Controllers/ConsultarDocumentoController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstREST.Controllers
+{
+    public class ConsultarDocumentoController : Controller
+    {
+        //
+        // GET: /ConsultarDocumento/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
